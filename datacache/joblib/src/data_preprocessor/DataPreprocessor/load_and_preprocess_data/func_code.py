1fba803c894fd577ffaa1de3fd81f371a112652e
Marco Bischoff <marco.bischoff98@outlook.de>
2024-02-26 11:22:53 -0300

    integrated memoization and updated environment

--- datacache/joblib/src/data_preprocessor/DataPreprocessor/load_and_preprocess_data/func_code.py
+++ datacache/joblib/src/data_preprocessor/DataPreprocessor/load_and_preprocess_data/func_code.py
@@ -1,31 +1,4 @@
-import pandas as pd
-import numpy as np
-from joblib import Memory
-
-# Set up cache directory for memoization
-cachedir = "./datacache"  # Consider placing this outside your source code directory
-memory = Memory(cachedir, verbose=0)
-
-class DataPreprocessor:
-    @staticmethod
-    def adjust_df_for_lag_and_trim(df, lag):
-        """
-        Prepends NaN rows equal to 'lag' at the beginning of the DataFrame and 
-        trims the same number of rows from the end to maintain original length.
-        
-        Parameters:
-        - df: DataFrame to be adjusted.
-        - lag: Number of periods to lag the data.
-        
-        Returns:
-        - DataFrame with adjusted data.
-        """
-        nan_rows = pd.DataFrame(np.nan, index=np.arange(lag), columns=df.columns)
-        concatenated_df = pd.concat([nan_rows, df]).reset_index(drop=True)
-        trimmed_df = concatenated_df.iloc[:-lag]
-        trimmed_df.index = df.index
-        return trimmed_df
-    
+# first line: 29
     @staticmethod
     @memory.cache
     def load_and_preprocess_data(fileName):
